edc8d602bcbdbd9edc63693c28b12c230ea55fde
Jacqualine <Khomotso.mmogoba@gmail.com>
2023-07-15 10:50:29 -0700

    printing numbers in base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int n;
+	char alph;
+
+	for (n = 0; n <= 10; n++)
+		putchar('n' + 0);
+
+	for (alph = 'a'; alph <= 'f'; alph++)
+		putchar(alph);
+
+putchar('\n');
+return (0);
+}
