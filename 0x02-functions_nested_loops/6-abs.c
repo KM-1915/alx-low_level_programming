25839021cfc654e5a21fa91f3145819946a105f0
Jacqualine <Khomotso.mmogoba@gmail.com>
2023-07-18 21:17:47 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,16 @@
 #include "main.h"
 /**
  * _abs - computes the absolute value of an integer
- * @i: 
+ * @i: integer
  * Return: absolute value
  */
 int _abs(int i)
 {
+	int absv;
 
-
+	if (i < 0)
+	{
+		absv = i * -1;
+	}
+	return (absv);
+}
