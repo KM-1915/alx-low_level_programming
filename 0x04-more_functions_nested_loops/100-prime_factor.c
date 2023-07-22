e0858df489b027014b11348fcac31df1e1d844ea
Jacqualine <Khomotso.mmogoba@gmail.com>
2023-07-21 18:10:27 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - prints the largest prime factor of the number 612852475143
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int i;
+	long num = 612852475143;
+
