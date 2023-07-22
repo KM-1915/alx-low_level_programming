7555d35f74b1b101e62eccc7aaef94547da6cf5a
Jacqualine <Khomotso.mmogoba@gmail.com>
2023-07-21 19:14:02 -0700

    Write a program that finds and prints the largest prime factor of the number 612852475143, followed by a new line

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include<stdio.h>
+#include<math.h>
 /**
  * main - prints the largest prime factor of the number 612852475143
  *
@@ -9,14 +10,15 @@ int main(void)
 {
 	int i, large;
 	long num = 612852475143;
+	double square = sqrt(num);
 
-	for (i = 1; i <= sqrt(num); i++)
+	for (i = 1; i <= square; i++)
 	{
 		if ((num % i) == 0)
 		{
 			large = num / i;
 		}
 	}
-	_putchar("%d\n", large);
+	printf("%d\n", large);
 	return (0);
 }
