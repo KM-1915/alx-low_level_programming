cd9d24a81f4091c8d085ae39d8d12ea93fba1b84
Jacqualine <Khomotso.mmogoba@gmail.com>
2023-07-25 15:40:54 -0700

    Write a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+#include <stdlib.h>
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string being converted
+ * Return: If there are no numbers in the string, return 0
+ */
+int _atoi(char *s)
+{
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for 
