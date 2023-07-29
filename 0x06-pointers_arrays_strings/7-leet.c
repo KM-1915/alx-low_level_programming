d5a1ed17c72e9e84a47c4ca9d2807a9f2c55cbab
Jacqualine <Khomotso.mmogoba@gmail.com>
2023-07-28 18:15:17 -0700

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,9 +3,24 @@
 #include<string.h>
 /**
  * *leet - encodes a string into 1337
- *
- * Return: Always 0
+ * @p: string
+ * Return: capitalized string
  */
-char *leet(char *)
+char *leet(char *p)
 {
+	int a, b;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (a = 0; p[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (p[a] == s1[b])
+			{
+				p[a] = s2[b];
+			}
+		}
+	}
+	return (p);
 }
