16c5a66ae9ee1b52eed4e717fa18c20cd85a1eb9
--global <khomotso.mmogoba@gmail.com>
2023-08-01 15:43:02 -0700

    Write a function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include<stdio.h>
 /**
- * *_strchr - locates a character in a string
+ * _strchr - locates a character in a string
  * @s: string c comes from
  * @c: character
  * Return: pointer to the first occurrence of the character c in s
