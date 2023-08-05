9c6eab227dbadc4f0d7aa3208ed6699c022978b0
--global <khomotso.mmogoba@gmail.com>
2023-08-04 20:40:52 -0700

    argc argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,14 +5,13 @@
  * main - prints the minimum number of coins to make change
  * @argc: argument count
  * @argv: argument vector
- * @cents: amount of cents you need to give back
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
 	int cents, mincoins = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
