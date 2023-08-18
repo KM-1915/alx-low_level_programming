ff73db82a719e82aaed42a3ef1efff229e8bcd0f
--global <khomotso.mmogoba@gmail.com>
2023-08-17 19:42:20 -0700

    Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,14 +13,16 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list pars;
 
-	va_start(pars, n);
 	if (n == 0)
 	{
 		return(0);
 	}
+
+	va_start(pars, n);
 	for (i = 0; i < n; i++)
+	{
 		sum += va_arg(pars, int);
-	
+	}
 	va_end(pars);
 	return (sum);
 }
