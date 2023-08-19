13e83b340adc02a92822bd6d82e63f28b8bf8f6b
--global <khomotso.mmogoba@gmail.com>
2023-08-18 19:36:02 -0700

    Function Pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include"function_pointers.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * array_iterator - executes function given as parameter onall elements of array
+ * @array: an array
+ * @size: size of the array
+ * @pointer: pointer to the function you need to use
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t n = 0;
+
+	for (n = 0; n < size; n++)
+	{
+		action(array[n]);
+	}
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include"function_pointers.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * int_index - searches for an integer
+ * @array: array of elements
+ * @size: the number of elements in the array array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: index of the first element which cmp functio
+ * does not return. No element matches, return -1
+ * If size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int n;
+
+	if (size <= 0)
+	{
+		return(-1);
+	}
+	for (n = 0; n < size; n++)
+	{
+		if (cmp(array[n]) != 0)
+		{
+			return(n);
+		}
+	}
+	return(-1);
+}
